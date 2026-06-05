98d041c030d44816659fcd2de9af67f3cc5d5b1c
Yukio Yakushijin <yukio.yakushijin@sony.com>
2026-06-05 14:51:15 +0900

    fixed icon issue

---
+++ scripts/after-pack.cjs
@@ -0,0 +1,17 @@
+const path = require("path");
+const rcedit = require("rcedit");
+
+module.exports = async function afterPack(context) {
+  if (context.electronPlatformName !== "win32") {
+    return;
+  }
+
+  const exePath = path.join(
+    context.appOutDir,
+    `${context.packager.appInfo.productFilename}.exe`
+  );
+  const iconPath = path.join(context.packager.projectDir, "build", "icon.ico");
+
+  await rcedit(exePath, { icon: iconPath });
+  console.log("Embedded Windows icon:", exePath);
+};
